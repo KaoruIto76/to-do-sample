d13930c2678c58eef19dced42d8fa8d745a0cc2f
Kaeru <soccerxxkaeru@gmail.com>
2019-12-14 19:31:13 +0900

    add package object

--- app/lib/persistence/package.scala
+++ app/lib/persistence/package.scala
@@ -11,5 +11,7 @@ package object persistence {
   val default = onMySQL
   object onMySQL {
     implicit lazy val driver = slick.jdbc.MySQLProfile
+    object TodoRepository     extends TodoRepository
+    object CategoryRepository extends CategoryRepository
   }
 }
